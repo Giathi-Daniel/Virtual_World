e49e6671f15ebabfc3e6bdb525085c4d81e02d36
DANIEL GIATHI <giathidaniel252@gmail.com>
2023-12-14 20:06:25 +0300

    virtualEnv

--- js/math/graph.js
+++ js/math/graph.js
@@ -13,13 +13,56 @@ class Graph{
     }
 
     tryAddPoint(point){
-    if(!this.containsPoint(point)){
-        this.addPoint(point);
-        return true;
+        if(!this.containsPoint(point)){
+            this.addPoint(point);
+            return true;
+        }
+        return false;
     }
-    return false;
+
+    addSegment(seg){
+        this.segments.push(seg);
     }
-    
+
+    containsSegment(seg){
+        return this.segments.find((s) => s.equals(seg));
+    }
+
+    tryAddSegment(seg){
+        if(!this.containsSegment(seg) && !seg.p1.equals(seg.p2)){
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
+    removeSegment(seg){
+        this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
+    getSegmentsWithPoint(point){
+        const segs = []; //empty array
+        for (const seg of this.segments){
+            if(seg.includes(point)){
+                segs.push(seg);
+            }
+        }
+        return segs;
+    }
+
+    dispose(){
+        this.points.length = 0;
+        this.segments.length = 0;
+    }
+
+    removePoint(point){
+        const segs = this.getSegmentsWithPoint(point); //removing the segment automatically where the point has been removed 
+        for (const seg of segs){
+            this.removeSegment(seg);
+        }
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
     draw(ctx){
         for(const seg of this.segments){
             seg.draw(ctx);
